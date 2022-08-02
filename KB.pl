2d1125e16cfdb68be5f6154ebaeda08488313b39
maria elena zaza <m.zaza16@studenti.uniba.it>
2022-08-02 10:15:33 +0200

    Migliorato file KB

---
+++ KB.pl
@@ -0,0 +1,16 @@
+%---REGOLE---
+
+
+
+
+% studente(id, idoneo, destinazione).
+
+%---LISTA STUDENTI---
+studente(90678, si, 133).
+studente(90478, si, 556).
+studente(90633, no, null).
+studente(93678, si, 211).
+studente(91678, no, null).
+studente(90448, no, null).
+studente(90078, si, 325).
+studente(91258, si, 332).
