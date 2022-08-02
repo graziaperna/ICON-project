eb792571b802ba9a941e76c0bdd9d4b2e59c02fe
maria elena zaza <m.zaza16@studenti.uniba.it>
2022-08-02 19:04:00 +0200

    Aggiunti dati nel file KB

--- KB.pl
+++ KB.pl
@@ -1,10 +1,14 @@
 :- dynamic(studente/3).
+:- dynamic(dipartimento/3).
+:- dynamic(dipartimento/3).
+
 %---REGOLE---
 
-
-
-
 % studente(id, idoneo, destinazione).
+% dipartimento(facolta, indirizzo, coordinatore).
+% destinazione(codiceDestinazione, facolta, disponibilita).
+
+%---DATI---
 
 %---LISTA STUDENTI---
 studente(90678, si, 133).
@@ -15,3 +19,19 @@ studente(91678, no, null).
 studente(90448, no, null).
 studente(90078, si, 325).
 studente(91258, si, 332).
+
+%---LISTA DIPARTIMENTI---
+dipartimento(Lingue, Via_Rossi, Falco).
+dipartimento(Informatica, Via_Blu,  Bianchi).
+dipartimento(Medicina, Via_Gramsci, Dignazio).
+dipartimento(Arte, Via_Ruvo, Comero).
+dipartimento(Psicologia, Via_Baracca, Scioscia).
+dipartimento(Matematica, Via_Rorri, Valenzano).
+
+%---LISTA DESTINAZIONI---
+destinazione(133, 'Informatica', 2).
+destinazione(122, Lingue, 3).
+destinazione(455, Arte, 2).
+destinazione(332, Psicologia, 1).
+destinazione(556, Matematica, 1).
+destinazione(006, Medicina, 4).
