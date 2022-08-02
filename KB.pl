8953352397c3eb1d57dad09a5272f84571f928c6
graziaperna <g.perna4@studenti.uniba.it>
2022-08-02 19:37:30 +0200

    Aggiunte informazioni alla KB

--- KB.pl
+++ KB.pl
@@ -1,6 +1,6 @@
 :- dynamic(studente/3).
 :- dynamic(dipartimento/3).
-:- dynamic(dipartimento/3).
+:- dynamic(destinazione/3).
 
 %---REGOLE---
 
@@ -21,17 +21,17 @@ studente(90078, si, 325).
 studente(91258, si, 332).
 
 %---LISTA DIPARTIMENTI---
-dipartimento(Lingue, Via_Rossi, Falco).
-dipartimento(Informatica, Via_Blu,  Bianchi).
-dipartimento(Medicina, Via_Gramsci, Dignazio).
-dipartimento(Arte, Via_Ruvo, Comero).
-dipartimento(Psicologia, Via_Baracca, Scioscia).
-dipartimento(Matematica, Via_Rorri, Valenzano).
+dipartimento('Lingue', 'Via Rossi', 'Falco').
+dipartimento('Informatica', 'Via Blu',  'Bianchi').
+dipartimento('Medicina', 'Via Gramsci', 'Dignazio').
+dipartimento('Arte', 'Via Ruvo', 'Comero').
+dipartimento('Psicologia', 'Via Baracca', 'Scioscia').
+dipartimento('Matematica', 'Via Rorri', 'Valenzano').
 
 %---LISTA DESTINAZIONI---
 destinazione(133, 'Informatica', 2).
-destinazione(122, Lingue, 3).
-destinazione(455, Arte, 2).
-destinazione(332, Psicologia, 1).
-destinazione(556, Matematica, 1).
-destinazione(006, Medicina, 4).
+destinazione(122, 'Lingue', 3).
+destinazione(455, 'Arte', 2).
+destinazione(332, 'Psicologia', 1).
+destinazione(556, 'Matematica', 1).
+destinazione(006, 'Medicina', 4).
