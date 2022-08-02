3d493471119b99b37e406475c4105cf25aef1de3
graziaperna <g.perna4@studenti.uniba.it>
2022-08-02 17:55:56 +0200

    Implementate funzioni aggiuntive

--- studente.py
+++ studente.py
@@ -5,27 +5,120 @@ from tabulate import tabulate
 prolog = Prolog()
 prolog.consult("KB.pl") 
 
-#mostra la lista degli studenti 
+#mostra la lista completa degli studenti 
 def studentList():
     
     myTrueQuery= "studente(ID,IDONEO,DESTINAZIONE)"
-    generalList(myTrueQuery)
+    outputResult(myTrueQuery, True)
 
 #mostra la lista degli studenti idonei
 def suitableStudentList():
     
     myTrueQuery= "studente(ID,si,DESTINAZIONE)"
-    generalList(myTrueQuery)
+    outputResult(myTrueQuery, True)
     
+#mostra la lista degli studenti non idonei
 def notSuitableStudentList():
     
     myTrueQuery= "studente(ID,no,DESTINAZIONE)"
-    generalList(myTrueQuery)
+    outputResult(myTrueQuery, True)
     
-def generalList(myTrueQuery):
-    myList = list(prolog.query(myTrueQuery))
+#dato l'ID, cerca uno studente
+def findStudent():
+    
+    studentID = input("Inserisci la matricola dello studente che vuoi cercare:\n")
+    myTrueQuery= "studente("+str(studentID)+",IDONEO,DESTINAZIONE)"
+    outputResult(myTrueQuery, True)
+    
+#inserisce uno studente
+def addStudent():
+    
+    studentID = ""
+    
+    #controllo numero caratteri della matricola inserita
+    while(not 1 <= len(str(studentID)) <= 5):
+       
+        studentID = input("Inserisci matricola nuovo studente:\n")
+        queryCheck = "studente("+str(studentID)+",IDONEO,DESTINAZIONE)"
+        
+        if(not 1 <= len(str(studentID)) <= 5):
+            print("Valore inserito non valido. Max 5 caratteri!\n")
+    
+    #controllo presenza id nel database
+    if(not outputResult(queryCheck, False)):
 
-    print(tabulate(myList, headers='keys',numalign="center"))
+        studentSuitability = ""
+        
+        #controllo inserimento 'si' o 'no'
+        while(studentSuitability != 'si' and studentSuitability != 'no'):
+            
+            studentSuitability = input("Inserisci se e' idoneo o meno(si/no): ").lower()
+            
+            if(studentSuitability != 'si' and studentSuitability != 'no'):
+                
+                print("Valore inserito non valido, inserire \"si\" o \"no\".\n")
+                
+        studentDestination = input("Inserisci l'ID della destinazione: ")
+        
+        queryCheck = "studente("+str(studentID)+","+studentSuitability+","+studentDestination+")"
+        prolog.assertz(queryCheck)
+        
+        print("Studente inserito nel database.")
+        
+    else:
+        print("Studente gia' presente nel database.\n")
+       
+    
+#modifica un progetto esistente
+def modifyStudent():
+    
+    studentID = input("Inserisci matricola dello studente da modificare:\n")
+    queryCheck = "studente("+str(studentID)+",IDONEO,DESTINAZIONE)"
+    
+    #controllo presenza id nel database
+    if(outputResult(queryCheck, False)):
+        
+        prolog.retractall(queryCheck)
+        
+        studentSuitability = ""
+        
+        #controllo inserimento 'si' o 'no'
+        while(studentSuitability != 'si' and studentSuitability != 'no'):
+            
+            studentSuitability = input("Inserisci se e' idoneo o meno(si/no): ").lower()
+            
+            if(studentSuitability != 'si' and studentSuitability != 'no'):
+                
+                print("Valore inserito non valido, inserire \"si\" o \"no\".\n")
+                
+        studentDestination = input("Inserisci l'ID della destinazione: ")
+                
+        queryCheck = "studente("+str(studentID)+","+studentSuitability+","+studentDestination+")"
+        prolog.assertz(queryCheck)
+        
+        print("Studente modificato correttamente.")
+        
+    else:
+        print("Studente non presente nel database.\n")
+        
+        
+
+
+#restituisce risultati query    
+def outputResult(myTrueQuery, printable):
+    
+    myList = list(prolog.query(myTrueQuery))
+    
+    if not myList:
+        if printable:
+            print("Nessun risultato trovato.\n") 
+        return False
+    
+    else:
+        if printable:
+            print(tabulate(myList, headers='keys', tablefmt="pretty", numalign="center"))
+        return True
+    
  
         
         
